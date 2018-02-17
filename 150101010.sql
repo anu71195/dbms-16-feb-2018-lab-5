bf376628d1a21f33d2372f2ce2846d4574d902b7
anu <anu71195@gmail.com>
2018-02-17 10:32:43 +0530

    csv files for scheduledin.csv has been generated

---
+++ 150101010.sql
@@ -0,0 +1,15 @@
+
+a)
+
+SELECT course_id FROM ScheduledIn
+WHERE room_number='2001';
+
+b)
+
+SELECT course_id FROM ScheduledIn
+WHERE letter='C';
+
+c)
+
+SELECT division FROM ScheduledIn
+WHERE room_number=
